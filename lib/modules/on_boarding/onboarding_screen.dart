5aa6148d4d7a9b18d80cf0fc8aa4640f94697c2f
eslamyounis1 <eslamupwork@gmail.com>
2022-08-12 09:43:51 +0200

    commit

--- lib/modules/on_boarding/onboarding_screen.dart
+++ lib/modules/on_boarding/onboarding_screen.dart
@@ -9,7 +9,9 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('hi'),
+      ),
     );
   }
 }
