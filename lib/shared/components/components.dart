3e2b4cb3c2ce22cfef1fd19ed923d4e0830c6d10
eslamyounis1 <eslamupwork@gmail.com>
2022-08-14 10:53:58 +0200

    commit

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_app/shared/components/constants.dart';
 
 // Navigation methods
 void navigateAndFinish(context, widget) => Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(builder: (context) => widget),
       (route) => false,
+    );
+
+// default Buttons
+Widget primaryButton({
+  required String buttonTxt,
+  required Function onPressedButton,
+}) =>
+    Container(
+      width: 343.0,
+      height: 48.0,
+      decoration: BoxDecoration(
+        color: primaryColor,
+        borderRadius: BorderRadius.circular(25.0),
+        boxShadow: [
+          BoxShadow(
+            color: primaryButtShadow.withOpacity(0.25),
+            offset: const Offset(0.0, 4.0),
+            blurRadius: 8.0,
+            spreadRadius: 0.0,
+          ),
+        ],
+      ),
+      child: MaterialButton(
+        onPressed: () {
+          onPressedButton;
+        },
+        child: Text(
+          buttonTxt,
+
+        ),
+      ),
     );
---
+++ lib/shared/components/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:hexcolor/hexcolor.dart';
+
+// primary colors
+Color primaryColor = HexColor('#DB3022');
+
+// shadows color
+Color primaryButtShadow = HexColor('D32626');
