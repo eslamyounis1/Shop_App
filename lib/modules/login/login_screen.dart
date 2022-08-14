fa731a03b811170bd48d9c68ea532f2232206256
eslamyounis1 <eslamupwork@gmail.com>
2022-08-14 14:38:20 +0200

    login_screen_ui

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -20,57 +20,119 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontFamily: 'metropolis',
-                fontSize: 34.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            const SizedBox(
-              height: 73.0,
-            ),
-            primaryTextField(
-              hintTxt: 'Email',
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            primaryTextField(
-              hintTxt: 'Password',
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            Container(
-              width: 343.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const Text(
-                    'Forgot your password?',
+                    'Login',
                     style: TextStyle(
                       fontFamily: 'metropolis',
-                      fontStyle: FontStyle.normal,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
+                      fontSize: 34.0,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
                   const SizedBox(
-                    width: 6.0,
+                    height: 73.0,
+                  ),
+                  primaryTextField(
+                    hintTxt: 'Email',
+                  ),
+                  const SizedBox(
+                    height: 8.0,
+                  ),
+                  primaryTextField(
+                    hintTxt: 'Password',
+                  ),
+                  const SizedBox(
+                    height: 16.0,
                   ),
                   Container(
-                    child: const Image(
-                      width: 24.0,
-                      height: 24.0,
-                      image: AssetImage('assets/images/arrow.png'),
+                    width: 343.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Forgot your password?',
+                          style: TextStyle(
+                            fontFamily: 'metropolis',
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 6.0,
+                        ),
+                        Container(
+                          child: const Image(
+                            width: 24.0,
+                            height: 24.0,
+                            image: AssetImage('assets/images/arrow.png'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
+                  const SizedBox(
+                    height: 32.0,
+                  ),
+                  primaryButton(
+                    onPressedButton: () {},
+                    buttonTxt: 'LOGIN',
+                  ),
+
+
+
+
                 ],
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                // color: Colors.teal,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 160.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Or login with social account',
+                          style: TextStyle(
+                            fontFamily: 'metropolis',
+                            fontStyle: FontStyle.normal,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Image(
+                          image: AssetImage('assets/images/google.png'),
+                          width: 92.0,
+                          height: 64.0,
+                        ),
+                        Image(
+                          image: AssetImage('assets/images/facebook.png'),
+                          width: 92.0,
+                          height: 64.0,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
